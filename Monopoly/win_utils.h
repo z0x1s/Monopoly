6156d5a268777655597cd511cddc385bee0b84ca
ProgWD <nikpd777@mail.ru>
2018-08-24 12:41:53 +0500

    program worked

--- Monopoly/win_utils.h
+++ Monopoly/win_utils.h
@@ -8,24 +8,21 @@
 
 #include <SFML\System\String.hpp>
 
-namespace dbg
-{
-	#define D_SHOW 5
-	#define D_HIDE 0
-}
+
+#define D_SHOW 5
+#define D_HIDE 0
+
 
 namespace dbg
 {
-
-	static HWND cDebug;
 
 	void set_mode(int macros)
 	{
-		cDebug = GetConsoleWindow();
+		HWND cDebug = GetConsoleWindow();
 		ShowWindow(cDebug, macros);
 	}
 
-	void log(sf::String str, std::ofstream fst, const char end = '\n')
+	void log(sf::String str, std::ofstream &fst, const char end = '\n')
 	{
 		fst << str.toAnsiString() << end;
 	}
