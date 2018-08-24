45e5fc4168a5396686037f93f911be5ea7191c57
ProgWD <nikpd777@mail.ru>
2018-08-24 12:56:26 +0500

    add monopolymap

---
+++ Monopoly/MonopolyMap.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "MonopolyMap.h"
+
+
+void MonopolyMap::render(sf::RenderWindow * rw)
+{
+	rw->draw(map);
+}
+
+MonopolyMap::MonopolyMap()
+{
+	this->map_image.loadFromFile(RESOURCES_IMAGES + "monopoly_map.png");
+	this->map.setTexture(this->map_image);
+	this->map.setPosition(sf::Vector2f(0, 0));
+}
+
+
+MonopolyMap::~MonopolyMap()
+{
+}
